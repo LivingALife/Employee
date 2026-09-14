5ee0e63960b5e700eaa0de6b3a419d534f43caa5
unknown <23105201@usc.edu.ph>
2026-09-14 16:54:24 +0800

    initialize project

---
+++ src/version2/PieceWorkerEmployee.java
@@ -0,0 +1,118 @@
+package version1;
+
+public class PieceWorkerEmployee {
+
+    private int empID;
+    private String empName;
+    private int totalPiecesFinished;
+    private double ratePerPiece;
+
+    // Default Constructor
+    public PieceWorkerEmployee() {
+        empID = 0;
+        empName = "N/A";
+        totalPiecesFinished = 0;
+        ratePerPiece = 0;
+    }
+
+    // Partial Constructor
+    public PieceWorkerEmployee(int empID, String empName) {
+        this.empID = empID;
+        this.empName = empName;
+        totalPiecesFinished = 0;
+        ratePerPiece = 0;
+    }
+
+    // Full Constructor
+    public PieceWorkerEmployee(
+            int empID,
+            String empName,
+            int totalPiecesFinished,
+            double ratePerPiece) {
+
+        this.empID = empID;
+        this.empName = empName;
+        setTotalPiecesFinished(totalPiecesFinished);
+        setRatePerPiece(ratePerPiece);
+    }
+
+    // Getters and Setters
+    public int getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public int getTotalPiecesFinished() {
+        return totalPiecesFinished;
+    }
+
+    public void setTotalPiecesFinished(int totalPiecesFinished) {
+        if (totalPiecesFinished >= 0) {
+            this.totalPiecesFinished = totalPiecesFinished;
+        }
+    }
+
+    public double getRatePerPiece() {
+        return ratePerPiece;
+    }
+
+    public void setRatePerPiece(double ratePerPiece) {
+        if (ratePerPiece >= 0) {
+            this.ratePerPiece = ratePerPiece;
+        }
+    }
+
+    // Compute Salary
+    public double computeSalary() {
+
+        double basePay =
+                totalPiecesFinished * ratePerPiece;
+
+        int completeHundreds =
+                totalPiecesFinished / 100;
+
+        double bonusPay =
+                completeHundreds * (10 * ratePerPiece);
+
+        return basePay + bonusPay;
+    }
+
+    // Display Employee
+    public void displayPieceWorkerEmployee() {
+
+        System.out.printf(
+                "ID: %d | Name: %s | Pieces Finished: %d | "
+                        + "Rate/Piece: \u20B1%.2f%n",
+                empID,
+                empName,
+                totalPiecesFinished,
+                ratePerPiece
+        );
+    }
+
+    // toString
+    @Override
+    public String toString() {
+
+        return String.format(
+                "PieceWorkerEmployee [ID: %d, Name: %s, Pieces: %d, "
+                        + "Rate: \u20B1%.2f, Total Salary: \u20B1%,.2f]",
+                empID,
+                empName,
+                totalPiecesFinished,
+                ratePerPiece,
+                computeSalary()
+        );
+    }
+}
